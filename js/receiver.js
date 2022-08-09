6e08e6a638a6fbe579e37864418e3aeaf6945b48
tslarsen <mrtlinsd@gmail.com>
2022-08-08 19:00:27 -0700

    Update receiver.js

--- js/receiver.js
+++ js/receiver.js
@@ -103,6 +103,30 @@ playerManager.addEventListener(
     }
 });
 
+
+const ismhm1_0x0D = context.canDisplayType('audio/mp4', ' mhm1.0x0D');
+const ismhm1_0x0E = context.canDisplayType('audio/mp4', ' mhm1.0x0E');
+const ismhm1_0x12 = context.canDisplayType('audio/mp4', ' mhm1.0x12');
+const ismha1_0x0D = context.canDisplayType('audio/mp4', ' mha1.0x0D');
+const ismha1_0x0E = context.canDisplayType('audio/mp4', ' mha1.0x0E');
+const ismha1_0x12 = context.canDisplayType('audio/mp4', ' mha1.0x12');
+const isatmos = context.canDisplayType('audio/mp4; codecs=ec-3; spatialRendering=true');
+
+castDebugLogger.debug(LOG_RECEIVER_TAG, 'Tonni Test debug');
+castDebugLogger.info(LOG_RECEIVER_TAG, 'Tonni Test info');
+castDebugLogger.error(LOG_RECEIVER_TAG, 'Tonni Test error');
+
+console.log("------------------");
+console.log("Supported format: audio/mp4', ' mhm1.0x0D: " + ismhm1_0x0D);
+console.log("Supported format: audio/mp4', ' mhm1.0x0E: " + ismhm1_0x0E);
+console.log("Supported format: audio/mp4', ' mhm1.0x12: " + ismhm1_0x12);
+console.log("Supported format: audio/mp4', ' mha1.0x0D: " + ismha1_0x0D);
+console.log("Supported format: audio/mp4', ' mha1.0x0E: " + ismha1_0x0E);
+console.log("Supported format: audio/mp4', ' mha1.0x12: " + ismha1_0x12);
+console.log("Supported format: audio/mp4; codecs=ec-3; spatialRendering=true: " + isatmos);
+console.log("------------------");
+
+
 /*
  * Example analytics tracking implementation. To enable this functionality see
  * the implmentation and complete the TODO item in ./google_analytics.js. Once
